a06c0c9d06ba0e21fd6e10d09fbd2dc33534adc8
Danweel <danweel.the.kelpie@gmail.com>
2020-06-04 14:30:09 -0400

    Updated code

--- python_if/excercise1.py
+++ python_if/excercise1.py
@@ -1,8 +1,12 @@
-value = '6'
+value = '8'
 
 if value == '7':
     print('The value is 7')
+elif value == '8':
+    print('The value is 8')
+elif value == '9':
+    print('The value is 9')
 else:
-    print('The value is not 7')
+    print('The value is not the one we''re looking for')
     
 print('Finished!')
