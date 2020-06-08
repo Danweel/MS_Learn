dec0b520399700f4dcf3bca53bc9876cc15d5915
Danweel <danweel.the.kelpie@gmail.com>
2020-06-07 21:23:46 -0400

    Solved challenge one

---
+++ python_if/challenge.py
@@ -0,0 +1,11 @@
+response = input("Would you like to continue?")
+
+if response == "No":
+    print("Exiting")
+
+elif response == "Yes":
+    print("Continuing ...")
+    print("Complete!")
+
+else:
+    print("Please try again and respond with yes or no.")
