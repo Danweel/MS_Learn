a3fc108e942a581e1ec3c0426e4831d0f66b491d
Danweel <danweel.the.kelpie@gmail.com>
2020-06-17 22:03:23 -0400

    Finished and cleared out old excercises to labdiary, set up next challenge

---
+++ python_if/challege2.py
@@ -0,0 +1,36 @@
+'''
+       First Challenge        
+Second challenge
+               Third challenge
+fourth#fifth#sixth!
+        fourth
+        fifth
+        sixth
+'''
+
+
+first_value = '  FIRST challenge         '
+second_value = '-  second challenge  -'
+third_value = 'tH IR D-C HALLE NGE'
+
+fourth_value = 'fourth'
+fifth_value = 'fifth'
+sixth_value = 'sixth'
+
+# First challenge
+
+
+# Second challenge
+
+
+# Third challenge
+
+
+print(first_value)
+print(second_value)
+print(third_value)
+
+# Fourth challenge - use only the print() function (no f-strings)
+
+
+# Fifth challenge - use only a single print() function.  Create tabs and new lines using f-strings.
---
+++ python_if/excercise3.py
@@ -0,0 +1,26 @@
+
+'''
+medicine = 'Coughussin'
+dosage = 5
+duration = 4.5
+
+instructions = '{} - Take {} ML by mouth every {} hours'.format(medicine, dosage, duration)
+print(instructions)
+
+instructions = '{2} - Take {1} ML by mouth every {0} hours'.format(medicine, dosage, duration)
+print(instructions)
+
+instructions = '{medicine} - Take {dosage} ML by mouth every {duration} hours'.format(medicine = 'Sneezergen', dosage = 10, duration = 6)
+
+print(instructions)
+'''
+
+
+name = 'World'
+message = f'Hello, {name}.'
+print(message)
+
+count = 10
+value = 3.14
+message = f'Count to {count}.  Multiply by {value}.'
+print(message)
