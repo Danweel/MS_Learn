16c51e4529a11ed83c91657487b298d4c4771fbc
Danweel <danweel.the.kelpie@gmail.com>
2020-07-17 08:31:42 -0400

    datatypes ex_1

---
+++ py_numeric_operations/exercise1.py
@@ -0,0 +1,21 @@
+'''print(type('7'))
+print(type(7))
+print(type(7.1))
+'''
+
+print(isinstance('7', str))
+print(isinstance(7, int))
+print(isinstance(7.1, float))
+
+print(isinstance(7, str))
+print(isinstance('7', int))
+print(isinstance('7.1', float))
+
+x = 'a string'
+print(type(x))
+x = 7
+print(type(x))
+x = False 
+print(type(x))
+
+#the above ease of changing x is unique to python
