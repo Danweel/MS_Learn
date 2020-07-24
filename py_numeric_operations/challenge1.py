350286a499ff326903e0d597503ea902a8725fea
Danweel <danweel.the.kelpie@gmail.com>
2020-07-24 10:03:34 -0400

    Some trial and error on C/F conversion

--- py_numeric_operations/challenge1.py
+++ py_numeric_operations/challenge1.py
@@ -1,9 +1,46 @@
 #user prompt
-faren = input("What is the temperature in fahrenheit?")
+fahrenheit = input("What is the temperature in fahrenheit? ")
 
-if faren == type(faren,int):
-    celsius = (val - 32) * 5/9
-    print(celsius)
 
-else:
-    print("Please input a whole number.")
+try:
+    int(fahrenheit)
+
+except:
+    float(fahrenheit)
+
+    
+
+if int(fahrenheit) is False:
+    print(int(round(fahrenheit)) - 32) * 5/9
+    
+else: c = (fahrenheit - 32) * 5/9
+print(c)
+
+
+'''
+To check if o is an instance of str or any subclass of str, use isinstance (this would be the "canonical" way):
+
+if isinstance(o, str):
+To check if the type of o is exactly str (exclude subclasses):
+
+if type(o) is str:
+The following also works, and can be useful in some cases:
+
+if issubclass(type(o), str):
+See Built-in Functions in the Python Library Reference for relevant information.
+
+'''
+
+
+'''
+fahrenheit = input('What is the temperature in Fahrenheit?  ')
+
+if fahrenheit.isnumeric() == False:
+    print('Input is not a number.')
+    exit()
+
+fahrenheit = int(fahrenheit)
+
+celsius = int((fahrenheit - 32) * 5/9)
+print('Temperature in celsius is ' + str(celsius))
+'''
