aa83189cfaabcf46ef478a4b16fcdba0d6557d8e
Danweel <danweel.the.kelpie@gmail.com>
2020-07-29 11:36:20 -0400

    Begun challenge2

---
+++ py_numeric_operations/challenge2.py
@@ -0,0 +1,24 @@
+
+
+
+number1 = input("Please enter the first number: ")
+
+num1 = int(num1)
+
+
+
+
+else
+    
+    print("Please enter a number. ")
+
+
+operator = input("Which Operator? (* , / , +, -, %, //) ")
+
+
+
+
+
+num2 = input("Please enter the second number: ")
+
+print("the product of ")
